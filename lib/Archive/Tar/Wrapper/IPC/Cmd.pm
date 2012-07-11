a25b38c5acd7e4dd6fd87d78c95e9d0a3da78b92
KRZYSZTOF BIESZCZAD <bieszczad@cpan.org>
2012-07-11 18:41:16 +0000

    Import of BIESZCZAD/Archive-Tar-Wrapper-IPC-Cmd-0.002 from CPAN.
    gitpan-cpan-distribution: Archive-Tar-Wrapper-IPC-Cmd
    gitpan-cpan-version:      0.002
    gitpan-cpan-path:         BIESZCZAD/Archive-Tar-Wrapper-IPC-Cmd-0.002.tar.gz
    gitpan-cpan-author:       BIESZCZAD
    gitpan-cpan-maturity:     released

--- lib/Archive/Tar/Wrapper/IPC/Cmd.pm
+++ lib/Archive/Tar/Wrapper/IPC/Cmd.pm
@@ -511,5 +511,31 @@ sub ramdisk_unmount {
 
 1;
 
+__END__
+=pod
 
-1;
+=head1 NAME
+
+	Archive-Tar-Wrapper minus IPC::Run, IO::Pty
+
+=head1 VERSION
+
+version 0.002 
+
+=head1 DESCRIPTION
+
+	Archive::Tar::Wrapper::IPC::Cmd is a fork of Michael Schilli's L<Archive::Tar::Wrapper>.
+	Removed: IPC::Run, IO::Pty
+	Added:   IPC::Cmd
+	
+=head1 COPYRIGHT AND LICENSE
+	This software is copyright (c) 2012 by Krzysztof Bieszczad.
+
+	This is free software; you can redistribute it and/or modify it under
+	the same terms as the Perl 5 programming language system itself.
+
+
+
+=cut
+
+
